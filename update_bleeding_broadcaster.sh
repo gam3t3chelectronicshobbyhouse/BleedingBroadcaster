42d81adb8addb503fa8ccf50f340d6b2434552cd
gam3t3chelectronicshobbyhouse <gam3t3ch@gmail.com>
2025-05-09 22:34:51 -0700

    Update update_bleeding_broadcaster.sh

--- update_bleeding_broadcaster.sh
+++ update_bleeding_broadcaster.sh
@@ -4,10 +4,12 @@
 INSTALL_DIR="$HOME/BleedingBroadcaster"
 
 if [ -d "$INSTALL_DIR/.git" ]; then
-  cd "$INSTALL_DIR"
   echo "Checking for updates..."
+  cd "$INSTALL_DIR"
+  echo "Please close Bleeding Broadcaster before applying updates."
+  read -p "Press [Enter] to continue with update or Ctrl+C to cancel..."
   git pull
-  echo "Bleeding Broadcaster updated. Please restart the application if it's running."
+  echo "Bleeding Broadcaster has been updated. Please restart the application manually if it's open."
 else
-  echo "Git repo not found in $INSTALL_DIR. Please reinstall using the installer script."
+  echo " Git repo not found in $INSTALL_DIR. Please reinstall using the installer script."
 fi
