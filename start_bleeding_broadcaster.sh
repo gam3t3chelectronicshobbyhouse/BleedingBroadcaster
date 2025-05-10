8d9bc0194fc5bdb529514171f10b254b67906f07
gam3t3chelectronicshobbyhouse <gam3t3ch@gmail.com>
2025-05-10 14:59:16 -0700

    Create start_bleeding_broadcaster.sh

---
+++ start_bleeding_broadcaster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Launching Bleeding Broadcaster..." > /home/pi/bleeding_broadcaster.log
+cd /home/pi/BleedingBroadcaster
+/usr/bin/python3 bleedingbroadcaster.py >> /home/pi/bleeding_broadcaster.log 2>&1
+echo ""
+echo "Program exited. Check /home/pi/bleeding_broadcaster.log for errors."
+read -p "Press Enter to close this window..."
